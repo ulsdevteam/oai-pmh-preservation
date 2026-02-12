2a39274f79aa92e42224ef5a35de2bc8fdf095d9
Ojas Mishra <ojm15@pitt.edu>
2026-02-12 11:39:44 -0500

    refactor

--- auth.py
+++ auth.py
@@ -1,12 +1,15 @@
 import requests
 from lxml import etree
 from collections import namedtuple
+import httpx
 import truststore
+from urllib.parse import urljoin
+import time
 truststore.inject_into_ssl()
 
 user_agent_headers = {
 'User-Agent':
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0'
 
 }
 ConfigData = namedtuple('ConfigData', [
@@ -18,7 +21,7 @@ ConfigData = namedtuple('ConfigData', [
     'send_header'
 ])
 
-conf = ConfigData(
+_conf = ConfigData(
     login_uri = "https://isapapers.pitt.edu/cgi/users/login",
     login_uname_el = "//*[@id=\"login_username\"]",
     login_passwd_el = "//*[@id=\"login_password\"]",
@@ -26,21 +29,43 @@ conf = ConfigData(
     gather_header = "Set-Cookie",
     send_header = "Cookie"
 )
+
+
 def login(conf, uname, passwd):
     # given xpath to input element get the form field name
-    uname_xpath = f"{conf.login_uname_el}/@name" 
-    passwd_xpath = f"{conf.login_passwd_el}/@name" 
-    form_action_xpath = f"{conf.login_form_el}/@action"
-    page = requests.get(conf.login_uri, headers=user_agent_headers)
-    et = etree.fromstring(page.content)
+    uname_xpath = conf.login_uname_el
+    passwd_xpath = conf.login_passwd_el
+    form_action_xpath = conf.login_form_el
+    page = httpx.get(conf.login_uri, headers=user_agent_headers)
+    content = page.text
+    if page.status_code == 403:
+        print("Most likely Cloudflare issue.")
+        '''
+        stuff_start = content.index("a.src") + len("a.src = ")
+        stuff_end = content.index(";", stuff_start)
+        time.sleep(3)
+        cf_url = content[stuff_start:stuff_end].strip("'")
+        r = httpx.get(urljoin(conf.login_uri, cf_url))
+        print(r.headers, r.status_code)
+        '''
+    #print(page.headers)
+    if type(content) == bytes:
+        content = content.decode("utf-8")
+    #print(content)
+    page.raise_for_status()
+    et = etree.fromstring(content, parser=etree.HTMLParser())
     uname_form_name = et.xpath(uname_xpath)[0]
     passwd_form_name = et.xpath(passwd_xpath)[0]
-    form_action_uri = et.xpath(form_action_xpath, 
-        namespaces={'r': et.xpath("namespace-uri()")})[0]
-    #print(uname_form_name, passwd_form_name, form_action_uri)
+    #form_action_uri = et.xpath(form_action_xpath, 
+    #    namespaces={'r': et.xpath("namespace-uri()")})[0]
+    form_action_uri = et.xpath(form_action_xpath) 
+    #        namespaces={'r': et.xpath("namespace-uri()")})[0]
+    print(uname_form_name, passwd_form_name, form_action_uri)
     
-    post_obj = requests.post(requests.compat.urljoin(conf.login_uri,form_action_uri), data={uname_form_name: uname, 
+    post_ret = requests.post(requests.compat.urljoin(conf.login_uri,form_action_uri), data={uname_form_name: uname, 
             passwd_form_name: passwd})
+    post_ret.raise_for_status()
+    return {conf.send_header: post_ret.headers[conf.gather_header]}
     print(post_obj.content, post_obj.headers)
     
     
--- scythe_refactor.py
+++ scythe_refactor.py
@@ -1,8 +1,22 @@
-
+from auth import ConfigData, login
 import os
-import urllib3
+from urllib.parse import urlparse
 import logging
 from itertools import islice
+from datetime import datetime, date
+from oaipmh_scythe import Scythe
+import httpx
+import logging
+import sys
+from collections import defaultdict
+import shutil
+from lxml import etree
+logging.basicConfig(level = logging.INFO)
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+identifiers_being_updated = set()
+request_client = None
 
 try:
     import tomllib
@@ -10,9 +24,18 @@ except ImportError:
     try:
         import tomlli as tomllib
     except:
-        logging.err("toml library not found")
+        log.err("toml library not found")
         raise SystemExit(1)
 logger = logging
+
+def is_valid_url(url):
+    try:
+        res = urlparse(url)
+        return all([res.scheme, res.netloc])
+    except Exception as e:
+        print(e)
+        return False
+
 def preprocess_config(config):
     storage_dir = config.get("storage_directory")
     abspath = os.path.abspath
@@ -28,7 +51,7 @@ def load_config(conf_location = "config.txt"):
     last_run, today = load_last_run()
     with open(conf_location, "rb") as conf_file:
         conf = tomllib.load(conf_file)
-        used_default = {(k,v) for k, v in defaults
+        used_defaults = {(k,v) for k, v in defaults.items()
             if k not in conf}
         conf["last_run"], conf["today"] = last_run, today
         logging.info(f"conf provided: {conf}")
@@ -53,46 +76,115 @@ def update_last_run(state_location = "state.txt"):
     pass
 
 
-def authenticate_scythe(client):
+def authenticate_scythe(client, config):
+    if config["login_type"] == "basic":
+        client.client.auth = httpx.BasicAuth(
+                                config["username"], 
+                                config["password"])
+        return client
+    auth_conf = ConfigData(
+        login_uri = config["login_uri"],
+        login_uname_el = config["login_username_xpath"],
+        login_passwd_el = config["login_password_xpath"],
+        login_form_el = config["login_form_xpath"],
+        gather_header = "Set-Cookie",
+        send_header = "Cookie"
+    )
+    login_headers = login(auth_conf, config["username"],
+                                config["password"])
+    
     return client
 
 def fetch_metadata_records(scythe_client, metadata_format, 
-    from_ = None, to_ = None):
+    config):
     records = scythe_client.list_records(
-        metadata_prefix = metadata_format.format_prefix) 
+        metadata_prefix = metadata_format.metadataPrefix,
+        from_ = config["last_run"], until = config["today"]) 
     
     records = islice(records, config["limit_entries"])
     return records
   
-def save_metadata_records(records, metadata_format, config):
+
+def get_record_header_info(basepath, record):
     pathjoin = os.path.join
-    for record in records:
-        identifier = record.header.identifier  
-        identifier = identifier.replace(":", "_")
-        record_path = pathjoin(config["storage_directory"],
-                            identifier)
-        
-        metadata_file_path = pathjoin(record_path, 
-                f"{identifier}.{metadata_format}")  
-        if not os.path.exists(record_path):
-            os.makedirs(record_path, exist_ok = True)
-        with open(metadata_file_path, 'w', 
-                encoding='utf-8') as f:
-            f.write(str(record))  # Save as string for now
+
+    identifier = record.header.identifier  
+    identifier = identifier.replace(":", "_")
+    record_path = os.path.join(basepath, identifier)
+    return record_path, identifier
+def save_metadata_record(record, metadata_format, config):
+    pathjoin = os.path.join
+    metadata_format = metadata_format.metadataPrefix
     
-def save_metadata_files():
-    raise NotImplementedError
+    identifier = record.header.identifier  
+    identifier = identifier.replace(":", "_")
+    record_path = pathjoin(config["storage_directory"],
+                        identifier)
+    
+    metadata_file_path = pathjoin(record_path, 
+            f"{identifier}.{metadata_format}")  
+    if not os.path.exists(record_path):
+        os.makedirs(record_path)
+    elif identifier not in identifiers_being_updated:
+        shutil.rmtree(record_path)
+        os.makedirs(record_path)
+        identifiers_being_updated.add(identifier)
+    with open(metadata_file_path, 'w', 
+            encoding='utf-8') as f:
+        log.info(f"writing file: {metadata_file_path}")
+        f.write(str(record))  # Save as string for now
+    
+
+def extract_file_uris(record_xml, xpath_expr, ns_dict):
+    tree = etree.XML(str(record_xml))
+    print(ns_dict)
+    vals = tree.xpath(xpath_expr, namespaces=ns_dict)
+    print(vals, [is_valid_url(x) for x in vals])
+    return list(filter(is_valid_url, tree.xpath(xpath_expr, namespaces=ns_dict)))  
+    pass
+
+def get_default_http_client():
+    # return an authenticated httpx client
+    return httpx # placeholder: replace with authenticated client
+        
+def save_metadata_file(record, metadata_format, config):
+    record_path, _ = get_record_header_info(config["storage_directory"], record)
+    metadata_format = metadata_format.metadataPrefix
+    file_uris = extract_file_uris(record, config["xpath"], config["namespaces"])
+    print(file_uris)
+    for uri in file_uris:
+        logger.info(f"getting uri {uri}")
+        client = get_default_http_client()
+        try:
+            response = client.get(uri)
+            response.raise_for_status()
+        except Exception as e:
+            logger.warn(f"failed to get {uri} due to {e}")
+            continue
+        file_name = os.path.basename(uri)
+        file_path = os.path.join(record_path, "files", file_name)
+        with open(file_path, "w") as f:
+            logger.info(f"saving {uri} @ {file_path}")
+            f.write(response.text)
+        
 def main():
     config = load_config()
     preprocess_config(config)
     with Scythe(config["base_url"]) as scythe:
+        authenticate_scythe(scythe, config)
         formats = scythe.list_metadata_formats()
         for meta_format in formats:
             records = fetch_metadata_records(
                             scythe, 
                             meta_format,
-                            from_ = config["last_run"],
-                            to_ = config["today"])
+                            config)
+            for record in records:
+                save_metadata_record(record, meta_format, 
+                    config)
+                if meta_format.metadataPrefix == config["metadata_format"]:
+                    logger.info("saving metadata files")
+                    save_metadata_file(record, meta_format, config)
+                
 
 
 if __name__ == '__main__':
